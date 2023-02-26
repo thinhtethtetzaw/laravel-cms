bd8631219338902c3d045362052d3ce7aa1cad36
thinhtethtetzaw <thinhtethtetzaw778@gmail.com>
2023-02-26 23:01:44 +0630

    test github

--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -18,7 +18,7 @@
                                 No
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Title
+                                Title test
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Link
